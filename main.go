b2284bb85b2c78a0d1147bc87861ba3ec2a11363
MAESTRO <maestro@local>
2026-09-01 13:39:38 +0200

    [maestro] Add an --uppercase flag to main.go that prints the greeting in all capit
    Mission: 114a0efee39c
    Scores: reviewer=100

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 )
 
 const Version = "0.1.0"
@@ -11,9 +12,18 @@ func Greeting() string {
 	return "hello from go-testbed"
 }
 
+func FormatGreeting(uppercase bool) string {
+	greeting := Greeting()
+	if uppercase {
+		return strings.ToUpper(greeting)
+	}
+	return greeting
+}
+
 func main() {
 	version := flag.Bool("version", false, "print version")
 	quiet := flag.Bool("quiet", false, "suppress output")
+	uppercase := flag.Bool("uppercase", false, "print greeting in uppercase")
 	flag.Parse()
 
 	if *version {
@@ -21,6 +31,6 @@ func main() {
 		return
 	}
 	if !*quiet {
-		fmt.Println(Greeting())
+		fmt.Println(FormatGreeting(*uppercase))
 	}
 }
